941fc6f4833c31b9c28a38851acadf159db78b26
stephenluc <stephenluc96@gmail.com>
2018-01-06 18:10:13 -0500

    Minor Fixes

--- js/custom.js
+++ js/custom.js
@@ -74,7 +74,9 @@ $(document).ready(function() {
         // Animate the scroll to top
       $('.go-top').click(function(event) {
         event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 300);
+      $('html, body').animate({scrollTop: 0}, 0);
+      /* TODO: replace the animation with this once fixed doctype */
+      /* $('html, body').animate({scrollTop: 0}, 300); */
       })
 
 
